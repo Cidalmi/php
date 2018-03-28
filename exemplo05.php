78e54235b3981ee22842ebe964a54f30ee791328
Cidalmi <cidalmi@hotmail.com>
2018-03-28 19:33:40 -0300

    Create Exemplo06.php

--- exemplo05.php
+++ exemplo05.php
@@ -1,10 +1,21 @@
 <?php 
 
+$nome = "Mozart";
 
+function teste(){
+global $nome;
+echo $nome;
 
+}
 
+function teste2(){
 
+$nome = "João";
+echo $nome." agora no teste2";
 
+}
 
+teste();
+teste2();
 
  ?>
